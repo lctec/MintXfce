f1217eabb31afa7ea516de9ceed69c90c322d6dd
lctec <69732259+lctec@users.noreply.github.com>
2020-10-30 20:03:18 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,2 +1,8 @@
 apt update
-apt install htop guake qbittorrent -y
+
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub |  apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
+apt install -y google-chrome-stable
+
+apt install -y htop guake qbittorrent 
