fd66f175a6741a80d40fe7129a5c391046f4870f
lctec <69732259+lctec@users.noreply.github.com>
2020-11-07 18:39:06 -0300

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,7 +5,7 @@ wget -q -O - https://dl.google.com/linux/linux_signing_key.pub |  apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | tee /etc/apt/sources.list.d/google-chrome.list
 apt install -y google-chrome-stable
 
-apt install -y htop guake qbittorrent 
+apt install -y htop guake qbittorrent notepadqq
 
 ############### Conky ################
 apt install -y conky-all
